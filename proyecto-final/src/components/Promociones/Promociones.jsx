b41a6a701ae21bc7393dcb62d488d5956b14cd8c
Oct4vius <armando2445@outlook.com>
2022-12-14 16:56:46 -0400

    Modal al canjear
    -Modal añadido para decidir en que tienda canjear los puntos.

--- proyecto-final/src/components/Promociones/Promociones.jsx
+++ proyecto-final/src/components/Promociones/Promociones.jsx
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Container, Row, Col } from "react-bootstrap";
 import "./Promociones.css";
+import "./Modal.css";
 import articulo from "../../assets/images/articuloLlamativo.jpeg";
 import axios from "axios";
 
@@ -11,6 +12,8 @@ export default function Promociones() {
 
   const [points, setpoints] = useState(0);
   const [email, setemail] = useState("");
+
+  const [openModal, setOpenModal] = useState(false);
 
   const url = `http://localhost:4200/api/users/${id}`
 
@@ -49,14 +52,19 @@ export default function Promociones() {
                       Este es un articulo llamativo el cual se podra canjear con
                       una X cantidad de puntos en una tienda XYZ.
                     </Card.Text>
-                    <Button variant="primary" onClick={canjear} >Canjear articulo</Button>
+                    <Button variant="primary" onClick={()=>{setOpenModal(true)}} >Canjear articulo</Button>
                   </Card.Body>
                 </Card>
               </Col>
             ))}
           </Row>
+          
         </Container>
+
+        
       </div>
+
+      {openModal && <Modal/>}
     </div>
   );
 
@@ -73,5 +81,39 @@ export default function Promociones() {
     }).then(()=>{alert("Puntos canjeados")})
   
   }
+
+  function Modal() {
+    return (
+      <div className="modalBackground">
+        <div className="modalContainer">
+          <div className="titleCloseBtn">
+          <button onClick={()=> {setOpenModal(false)}}> X </button>
+          </div>
+          <div className="title">
+            <h1>Seleccione donde quiere canjear los puntos:</h1>
+          </div>
+          <div className="body">
+            <button className="button button1" onClick={()=>{
+              setOpenModal(false);
+              canjear();
+            }}>La Sirena</button>
+            <button className="button button2" onClick={()=>{
+              setOpenModal(false);
+              canjear();
+            }}>El Bravo</button>
+            <button className="button button3" onClick={()=>{
+              setOpenModal(false);
+              canjear();
+            }}>El Nacional</button>
+          </div>
+          <div className="footer">
+            <button onClick={()=> {setOpenModal(false)}} >Cancelar</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+
 }
 
