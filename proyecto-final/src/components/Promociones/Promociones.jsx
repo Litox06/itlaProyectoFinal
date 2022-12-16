5426bdf265a2b73132796e108d9c3009c20c1667
litox06 <litox06@gmail.com>
2022-12-15 22:33:08 -0500

    Arreglando imagenes en Promociones

--- proyecto-final/src/components/Promociones/Promociones.jsx
+++ proyecto-final/src/components/Promociones/Promociones.jsx
@@ -1,10 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Container, Row, Col } from "react-bootstrap";
 import "./Promociones.css";
 import "./Modal.css";
-import articulo from "../../assets/images/articuloLlamativo.jpeg";
+import smartFridge from "../../assets/images/articuloLlamativo.jpeg";
+import amgSUV from "../../assets/images/amgSUV.png"
+import smartTV from "../../assets/images/sonyBravia.jpeg"
 import axios from "axios";
 
 export default function Promociones() {
@@ -15,12 +17,12 @@ export default function Promociones() {
 
   const [openModal, setOpenModal] = useState(false);
 
-  const url = `http://localhost:4200/api/users/${id}`
+  const url = `http://localhost:4200/api/users/${id}`;
 
-  axios.get(url).then(response =>{
-    setpoints(response.data.puntos)
-    setemail(response.data.email)
-  })
+  axios.get(url).then((response) => {
+    setpoints(response.data.puntos);
+    setemail(response.data.email);
+  });
 
   return (
     <div className="home-container">
@@ -32,55 +34,80 @@ export default function Promociones() {
       <div className="articulos">
         <Container>
           <Row>
-
-            {[{title: "hi", body: ""}, 1, 2].map((i) => (
+            {[
+              {
+                title: "Samsung RF23A9771",
+                body: "El Smart Counter Depth 4-Door Flex™ con Family Hub™ te ayuda a conectar con lo más importante: tu familia y tu hogar.",
+                article: smartFridge ,
+                widthImage: 100,
+                heightImage: 180,
+              },
+              {
+                title: "Sony BRAVIA OLED",
+                body: "Disfrute de una experiencia visual con visión y sonido en perfecta armonía, gracias al procesador cognitivo inteligente XR.",
+                article: smartTV,
+                widthImage: 250,
+                heightImage: 180,
+              },
+              {
+                title: "2022 AMG G63 SUV",
+                body: "Con 577 caballos artesanales, el AMG G63 es una leyenda elevada a una potencia superior para una nueva era.",
+                article: amgSUV,
+                widthImage: 230,
+                heightImage: 180,
+              },
+            ].map((i) => (
               <Col md="4">
                 <Card style={{ width: "18rem", marginRight: 40 }}>
                   <Card.Img
                     variant="top"
                     className="rounded mx-auto d-block"
-                    src={articulo}
+                    src={i.article}
                     style={{
                       padding: 20,
-                      width: 100,
-                      height: 180,
+                      width: i.widthImage,
+                      height: i.heightImage,
                       justifyContent: "center",
                     }}
                   />
                   <Card.Body>
-                    <Card.Title>{i.text}</Card.Title>
-                    <Card.Text>
-                      Este es un articulo llamativo el cual se podra canjear con
-                      una X cantidad de puntos en una tienda XYZ.
-                    </Card.Text>
-                    <Button variant="primary" onClick={()=>{setOpenModal(true)}} >Canjear articulo</Button>
+                    <Card.Title>{i.title}</Card.Title>
+                    <Card.Text>{i.body}</Card.Text>
+                    <Button
+                      variant="primary"
+                      onClick={() => {
+                        setOpenModal(true);
+                      }}
+                    >
+                      Canjear articulo
+                    </Button>
                   </Card.Body>
                 </Card>
               </Col>
             ))}
           </Row>
-          
         </Container>
-
-        
       </div>
 
-      {openModal && <Modal/>}
+      {openModal && <Modal />}
     </div>
   );
 
-  async function canjear(){
-  
-    let p = points - 50
+  async function canjear() {
+    let p = points - 50;
 
-    await axios.put(url,{
-      puntos: p
-    })
+    await axios.put(url, {
+      puntos: p,
+    });
 
-    axios.get(url).then(response =>{
-      setpoints(response.data.puntos)
-    }).then(()=>{alert("Puntos canjeados")})
-  
+    axios
+      .get(url)
+      .then((response) => {
+        setpoints(response.data.puntos);
+      })
+      .then(() => {
+        alert("Puntos canjeados");
+      });
   }
 
   function Modal() {
@@ -88,33 +115,58 @@ export default function Promociones() {
       <div className="modalBackground">
         <div className="modalContainer">
           <div className="titleCloseBtn">
-          <button onClick={()=> {setOpenModal(false)}}> X </button>
+            <button
+              onClick={() => {
+                setOpenModal(false);
+              }}
+            >
+              {" "}
+              X{" "}
+            </button>
           </div>
           <div className="title">
             <h1>Deseo canjear mi articulo en:</h1>
           </div>
           <div className="body">
-            <button className="button button1" onClick={()=>{
-              setOpenModal(false);
-              canjear();
-            }}>La Sirena</button>
-            <button className="button button2" onClick={()=>{
-              setOpenModal(false);
-              canjear();
-            }}>El Bravo</button>
-            <button className="button button3" onClick={()=>{
-              setOpenModal(false);
-              canjear();
-            }}>El Nacional</button>
+            <button
+              className="button button1"
+              onClick={() => {
+                setOpenModal(false);
+                canjear();
+              }}
+            >
+              La Sirena
+            </button>
+            <button
+              className="button button2"
+              onClick={() => {
+                setOpenModal(false);
+                canjear();
+              }}
+            >
+              El Bravo
+            </button>
+            <button
+              className="button button3"
+              onClick={() => {
+                setOpenModal(false);
+                canjear();
+              }}
+            >
+              El Nacional
+            </button>
           </div>
           <div className="footer">
-            <button onClick={()=> {setOpenModal(false)}} >Cancelar</button>
+            <button
+              onClick={() => {
+                setOpenModal(false);
+              }}
+            >
+              Cancelar
+            </button>
           </div>
         </div>
       </div>
     );
   }
-  
-
 }
-
